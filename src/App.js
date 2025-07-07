b2cd69e42d36223576771e1ce9621e2234eb344a
Lalit210399 <lalit.jadhav@candentech.com>
2025-07-07 11:37:09 +0530

    protected route logic is implemented

--- src/App.js
+++ src/App.js
@@ -11,38 +11,40 @@ import StepForm from "./CommonComponents/MultiStepForm";
 import EventDetailPage from "./Pages/Task/EventDetailPage";
 import ForgotPassword from "./CommonComponents/UserAuth/ForgotPassword";
 import TasksDetail from "./Pages/Task/TaskDetailPage";
-import Instagram from "./InstagramPost"; // Assuming this is the correct path for Instagram component
+import Instagram from "./InstagramPost";
+import ProtectedRoute from "./Context/ProtectedRoute"; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Routes that don't use MainLayout */}
+        {/* Public Routes */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
-        {/* Routes that use MainLayout */}
+        {/* Protected Routes */}
         <Route
           path="/*"
           element={
-            <MainLayout>
-              <Routes>
-                <Route path="/auth" element={<AuthN />} />
-                <Route path="/events" element={<Event />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/schedule" element={<Schedule />} />
-                <Route path="/events/stepForm" element={<StepForm />} />
-                <Route path="/dashboard/stepForm" element={<StepForm />} />
-                <Route path="/schedule/stepForm" element={<StepForm />} />
-                <Route path="/events/eventDetailPage" element={<EventDetailPage />} />
-                <Route path="/events/eventDetailPage/tasks" element={<TasksDetail />} />
-                <Route path="/instagram" element={< Instagram/>} />
-                
-                {/* Add more routes as needed */}
-              </Routes>
-            </MainLayout>
+            <ProtectedRoute>
+              <MainLayout>
+                <Routes>
+                  <Route path="/auth" element={<AuthN />} />
+                  <Route path="/events" element={<Event />} />
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/schedule" element={<Schedule />} />
+                  <Route path="/events/stepForm" element={<StepForm />} />
+                  <Route path="/dashboard/stepForm" element={<StepForm />} />
+                  <Route path="/schedule/stepForm" element={<StepForm />} />
+                  <Route path="/events/eventDetailPage" element={<EventDetailPage />} />
+                  <Route path="/events/eventDetailPage/tasks" element={<TasksDetail />} />
+                  <Route path="/instagram" element={<Instagram />} />
+                  {/* Add more protected routes here */}
+                </Routes>
+              </MainLayout>
+            </ProtectedRoute>
           }
         />
       </Routes>
