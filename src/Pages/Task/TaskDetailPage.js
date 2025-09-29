3b1092d614a94b13ec69b334c9bf831cf9397746
hrku <chinum.upadhyaya@gmail.com>
2025-09-29 21:23:36 +0530

    sample

--- src/Pages/Task/TaskDetailPage.js
+++ src/Pages/Task/TaskDetailPage.js
@@ -1178,7 +1178,6 @@ const TaskDetailPage = () => {
           onStatusChange={handleStatusChange}
           isUpdatingStatus={isUpdatingStatus}
           user={user}
-          createdBy={createdBy}
         />
       </div>
 
