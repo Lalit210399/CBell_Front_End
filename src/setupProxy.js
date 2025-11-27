f85740dbfd6aecf016b12842abc8adfb230bf0a7
Lalit210399 <lalit.jadhav@candentech.com>
2025-11-27 17:47:55 +0530

    minor fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     '/apis',
     createProxyMiddleware({
-      target: 'https://sincere-starfish-mutually.ngrok-free.app',
+      target: 'https://cbell.ai/apis',
       changeOrigin: true,
       ws: true, 
       pathRewrite: { '^/apis': '' },
